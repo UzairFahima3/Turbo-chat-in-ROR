9e841c841db3a1a8ffce38ba50dc929f733ff05f
UzairFahima3 <uzairfahima3@gmail.com>
2022-09-12 15:38:17 +0500

    Bootstrap of css and navbar is added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+
+  scope :all_except, ->(user) { where.not(id: user)}
+
+
+ end
